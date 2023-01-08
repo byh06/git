23d0414abd553916d7df3cb7d2b9d80d183d956a
imjh1031 <calla_letter@naver.com>
2023-01-08 11:40:36 +0900

    0108 study

---
+++ javastudy-20221217-byh/src/j01_출력/PrintTest.java
@@ -0,0 +1,30 @@
+package j01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello, Java!");
+		 
+		 System.out.print("변윤후");
+		 System.out.print("-----");
+		 
+		 System.out.print("주말수업");
+		 System.out.println("--");
+		 
+		 // 주석 : 프로그램에 영향을 주지 않는 글
+//		 실행 단축기 : CTRL F11
+//   	주석 단축기 : ctrl +/
+//		 println 은 줄 바꿈해라 이다
+		 
+		 System.out.print("Hello, \nJava\n");
+		 System.out.println("변윤후");
+//		 \n : 줄바꿈
+		 
+		 int level;
+		 level = 10;
+		 System.out.println(level);
+		 level = 11;
+		 System.out.println(level);
+	}
+
+}
---
+++ javastudy-20221217-byh/src/j02_변수와상수/Constant.java
@@ -0,0 +1,18 @@
+package j02_변수와상수;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		int maxNumber = 100;
+		int minNumber = 0;
+		
+		int number = 10;
+		
+		number = 11;
+		System.out.println("최댓값: " + maxNumber);
+		System.out.println("최솟값: " + minNumber);
+		System.out.println("현재값" + number);
+		
+	}
+
+}
